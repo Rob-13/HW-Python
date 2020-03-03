d786487f69e255c6084b749698d7472b8d4bc3ad
Rob <rob-13@mail.ru>
2020-03-03 18:44:35 +0300

    Первая работа над ошибками

--- Lesson2/task4.py
+++ Lesson2/task4.py
@@ -1,7 +1,4 @@
 #str1 = input("Введите свою строку: ")
 str1 = 'Ну ничего себе я попал на курс программирования'
-i = 1
-for item in str1.split():
-    print(f'{i} - {item[:10]}')
-    i += 1
-
+for item in enumerate(str1.split()):
+    print(f'{item[0]} - {item[1][:10]}')
---
+++ test.py
@@ -0,0 +1,6 @@
+from math import pi
+r = 15
+
+area = pi * r ** 2
+print(round(area, 2))
+
